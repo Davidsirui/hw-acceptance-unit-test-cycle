3c0a2ce9a256cd3d2c813da7974265342aea0fab
EC2 Default User <ec2-user@ip-172-31-82-46.ec2.internal>
2021-03-11 07:56:42 +0000

    HW4 FINISHED WITH 95% CODE COVERAGE

--- rottenpotatoes/spec/controller/controller_spec.rb
+++ rottenpotatoes/spec/controller/controller_spec.rb
@@ -15,11 +15,19 @@ describe MoviesController, type: :controller do
         response.should redirect_to movies_path
     end
     
-    it "returns similar movies with the same director" do
+    it "When the specified movie has a director, return similar movies with the same director" do
         movie1 = Movie.create(title: 'Catch me if you can', director: 'Steven Spielberg')
-        movie2 = Movie.create(title: 'Seven', director: 'David Fincher')
-        movie3 = Movie.create(title: "Schindler's List", director: 'Steven Spielberg')
-        Movie.search_similar_movies('Steven Spielberg').should eq [movie1, movie3]
+        movie2 = Movie.create(title: "Schindler's List", director: 'Steven Spielberg')
+        get :search_directors, {:id => movie1.id}
+        expect(response).to render_template(:similar_movies)
+        assigns(:movie).should eq movie1
+    end
+    
+    it "When the specified movie has no director, redirct to movie path" do
+        movie1 = Movie.create(title: 'No director', director: '')
+        get :search_directors, {:id => movie1.to_param}
+        expect(response).to redirect_to(movies_path)
+        assigns(:movie).should eq movie1
     end
     
 end
--- rottenpotatoes/spec/model/models_spec.rb
+++ rottenpotatoes/spec/model/models_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-# describe 'find all movies with the same director' do
-#   it "returns similar movies with the same director" do
-#     movie1 = Movie.create(title: 'Catch me if you can', director: 'Steven Spielberg')
-#     movie2 = Movie.create(title: 'Seven', director: 'David Fincher')
-#     movie3 = Movie.create(title: "Schindler's List", director: 'Steven Spielberg')
-#     Movie.search_similar_movies('Steven Spielberg').should eq [movie1, movie3]
-#   end
-# end
+describe 'find all movies with the same director' do
+  it "returns similar movies with the same director" do
+    movie1 = Movie.create(title: 'Catch me if you can', director: 'Steven Spielberg')
+    movie2 = Movie.create(title: 'Seven', director: 'David Fincher')
+    movie3 = Movie.create(title: "Schindler's List", director: 'Steven Spielberg')
+    Movie.search_similar_movies('Steven Spielberg').should eq [movie1, movie3]
+  end
+  
+  it "should not return similar movies with the same director" do
+    movie1 = Movie.create(title: 'Catch me if you can', director: 'Steven Spielberg')
+    movie2 = Movie.create(title: 'Seven', director: 'David Fincher')
+    movie3 = Movie.create(title: "Schindler's List", director: 'Steven Spielberg')
+    expect(Movie.search_similar_movies('Steven Spielberg')).not_to eq [movie2]
+  end
+end
