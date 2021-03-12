672a3cc9f909b5d638fdf67b02ff3bdf597276e9
EC2 Default User <ec2-user@ip-172-31-82-46.ec2.internal>
2021-03-12 00:33:55 +0000

    HW4 finished with 95% code coverage and HW3 code included

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -22,18 +22,43 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  expect(page.body.index(e1) < page.body.index(e2))
+  # expect(page.body.index(e1) < page.body.index(e2))
+  
+  first = (/#{e1}/ =~ page.body)
+  second = (/#{e2}/ =~ page.body)
+  first.should <= second
+end
+
+Then /^I should not see : (.*)/ do |movies|
+  movies_a = movies.split( "," )
+  movies_a.each do |movie|
+    m = movie.strip
+    step( "I should not see #{m}" )
+  end
 end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+  # rating_list.split(', ').each do |rating|
+  #   step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+  # end
+  
+  ratings = rating_list.split(", ")
+  
+  ratings.each do |r|
+    if(uncheck == 'un')
+      uncheck("ratings_#{r}")
+    else
+      check("ratings_#{r}")
+    end
   end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  Movie.all.each do |movie|
-    step %{I should see "#{movie.title}"}
-  end
+  # Movie.all.each do |movie|
+  #   step %{I should see "#{movie.title}"}
+  # end
+  
+  rows = Movie.count
+  rows.should == 10
 end
